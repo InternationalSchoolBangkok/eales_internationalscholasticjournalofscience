ae698edcd075f77665ae3d12379e40c1d2393a22
Nico <ramicaza@gmail.com>
2015-02-03 18:41:37 +0700

    PHPd it up
    luv php

---
+++ New Website/index.php
@@ -0,0 +1,302 @@
+<head>
+	<title>Journal of Science</title>
+	<link rel="stylesheet" type="text/css" href="css/navigation.css">
+	<link rel="stylesheet" type="text/css" href="css/body.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>
+	function onLoad(){
+		reloadPage();
+	}
+	function reloadPage(){
+		setTimeout("reloadPage();", 1000);
+		$('#date').load(document.URL +  ' #date');
+		//alert("awooga");
+	}
+	</script>
+	</head>
+	<body onload="onLoad();">
+		<nav class="globalheader">
+			<ul>
+
+				<li>
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="contact.html">Contact Us</a>
+				</li>
+				<li>
+					<a href="editorial.html">Editorial Policy</a>
+
+				</li>
+				<li>
+					<a>Documents <span>&#x25BC;</span></a><!-- dropdown arrow-->
+					<ul>
+						<li class="header">
+							ISJoS
+						</li>
+						<li>
+							<a href="">2015</a>
+							<ul>
+								<li>
+									<a href="">Master Han Report</a>
+								</li>
+							</ul>
+						</li>
+						<li class="header">
+							ISB JoS
+						</li>
+						<li>
+							<a href="">2014 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=CoR+of+Wet+Tennis+Ball">CoR of Wet Tennis Ball</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Conga+Sound+Quality">Conga Sound Quality</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Force+on+a+Landing+Cat">Force on a Landing Cat</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Seed+Dispersal">Seed Dispersal</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2013 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Ball+on+a+Plate">Ball on a Plate</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=CoR+of+Ball">CoR of Ball</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Ball+Spin+and+Deflection">Ball Spin and Deflection</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Badminton+Dropshot">Badminton Dropshot</a>
+								</li>
+							</ul>
+						</li>
+						<li class="header">
+							ISB JoP
+						</li>
+						<li>
+							<a href="">2012 Issue 2</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Spherical+Harmonics+in+a+Whistle">Spherical Harmonics in a Whistle</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Impulse+of+a+Kendo+Strike">Impulse of a Kendo Strike</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2012 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Drum+Sound+Intensity">Drum Sound Intensity</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Resonance+in+Bottles">Resonance in Bottles</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2011 Issue 2</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Cone-Topped+Tube">Cone-Topped Tube</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Spherical+Resonator">Spherical Resonator</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Length+and+Flow+Rate">Length and Flow Rate</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Can+Telephone">Can Telephone</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2011 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Diameter+and+End+Correction">Diameter and End Correction</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Resonance+in+a+Bottle">Resonance in a Bottle</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Temperature+of+a+Tuning+Fork">Temperature of a Tuning Fork</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2010 Issue 2</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Deceleration+of+a+Shuttlecock">Deceleration of a Shuttlecock</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Double+Ball+Bounce">Double Ball Bounce</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2010 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=LED+Resistance+and+Temperature">LED Resistance and Temperature</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Crater+Formation+in+Water">Crater Formation in Water</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Superball+Temperature+and+Coefficient+of+Restitution">Superball Temperature and Coefficient of Restitution</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2009 Issue 2</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Velocity+of+a+Golf+Putt">Velocity of a Golf Putt</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Ball+Pressure+and+Coefficient+of+Restitution">Ball Pressure and Coefficient of Restitution</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Increasing+Collision+Time+Through+the+Use+of+Cushioning">Increasing Collision Time Through the Use of Cushioning</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=The+Glowing+Pickle+and+Other+Vegetables">The Glowing Pickle and Other Vegetables</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2009 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Turbulator+Diameter+and+Drag+on+a+Sphere">Turbulator Diameter and Drag on a Sphere</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Wavelength+and+End+Correction+in+a+Recorder">Wavelength and End Correction in a Recorder</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2008 Issue 2</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Seiche+Waves+in+Deep+and+Shallow+Water">Seiche Waves in Deep and Shallow Water</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Velocity+of+a+Paintball+in+Air">Velocity of a Paintball in Air</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=LED+Intensity+as+a+function+of+Input+Power">LED Intensity as a function of Input Power</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2008 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Water+Oscillation+in+an+Open+Tube">Water Oscillation in an Open Tube</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Effectiveness+of+Nivea+Sunscreen+under+Ultraviolet+Light">Effectiveness of Nivea Sunscreen under Ultraviolet Light</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="">2007 Issue 1</a>
+							<ul>
+								<li>
+									<a href="http://isjos.org/?paper=Formation+of+Impact+Craters+in+Sand">Formation of Craters In Sand</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Coefficient+of+Restitution+of+a+Tennis+Ball">Coefficient of Restitution of a Tennis Ball</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Mass+of+an+Air+Cannon+Vortex">Mass of an Air Cannon Vortex</a>
+								</li>
+								<li>
+									<a href="http://isjos.org/?paper=Spark+Length+and+the+Van+de+Graaff+Generator">Spark Length and the Van de Graaff Generator</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					
+				</li>
+			</ul>
+		</nav>
+	<?php
+	parse_str($_SERVER['QUERY_STRING'], $queryParams);
+	//commented section prints ALL parameters in GET
+	//print_r($queryParams);
+	//echo $queryParams["paper"];
+	$paperName = $queryParams["paper"];
+	$paperLocation = "";
+	if($paperName==""): ?>
+		<div class="maincontent">
+			<div class="header">
+				Interscholastic Journal of Science
+			</div>
+			<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				An important component of the Science program at International School Bangkok is experimental research. All students conduct and report on several pieces of original research during the course of their studies. The ISB Journal of Science provides a forum in which ISB students may publish their research. The papers have been peer reviewed and have been judged correct and substantive.
+			</p>
+			<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				The Journal was founded in 2007 as the Journal of Physics by Mr. Jonathan Eales. Mr. Jonathan Eales and Dr. Ian Jacobs were the Editors of the Journal of Physics from 2007 until 2012. The Journal of Physics was published semi-annually in January and June.
+			</p>
+			<p>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				In January of 2013, the Journal was expanded to accept papers from all the sciences and was renamed the ISB Journal of Science. The Journal of Science is edited by Dr. Jonathan Eales. The Peer Review Board consists of current and former secondary school science teachers. The Journal of Science publishes papers on a rolling basis as they are received throughout the year. Articles in the Journal are copyrighted under Creative Commons licensing. The Journal is listed in the Directory of Open Access Journals.
+			</p>
+		</div>
+	<?php 
+	/*libxml_use_internal_errors(true);
+	$xml = simplexml_load_file("mapFile.xml") or die("Error: Cannot create object");
+	if ($xml === false) {
+		echo "Failed loading XML: ";
+		foreach(libxml_get_errors() as $error) {
+			echo "<br>", $error->message;
+		}
+	} else {
+		foreach($xml->item as $item){
+			echo "<a href=\"/?paper=",urlencode($item->name),"\">",$item->name,"</a></br>";
+			//echo $item->location,$item->name;
+		}
+	}*/
+	?>
+<?php else: 
+	libxml_use_internal_errors(true);
+	$xml = simplexml_load_file("mapFile.xml") or die("Error: Cannot create object");
+	if ($xml === false) {
+		echo "Failed loading XML: ";
+		foreach(libxml_get_errors() as $error) {
+			echo "<br>", $error->message;
+		}
+	} else {
+		foreach($xml->item as $item){
+			//Now you can access the 'row' data using $Item in this case 
+			//two elements, a name and an array of key/value pairs
+			//echo $item->name,"	",$item->location,"</br>";
+			if($paperName == $item->name){
+				//echo $item->location;
+				$paperLocation = $item->location;
+			}
+		}
+		//print_r($xml);	
+	}
+	?>
+		<iframe src="http://docs.google.com/gview?url=http://isjos.org<?php echo $paperLocation?>&embedded=true" style="width:100%; height:100%;" frameborder="0"></iframe>
+<?php endif ?>
+</body>
+</html>
